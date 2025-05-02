f4c34c2248cfe543a34a84f9c2d743ae7d029260
Nick Yuen <138299516+NerdyClimber6969@users.noreply.github.com>
2025-05-02 11:33:40 +0800

    Add authentication guard
    - if user logged in and access the login or sign up page, it will be redirect
    to the home page.

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,21 +1,32 @@
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuthen } from '../../context/AuthenProvider.jsx';
 import LoginForm from '../../components/LoginForm/LoginForm.jsx';
 import styles from './LoginPage.module.css';
 
 function LoginPage(props) {
+    const { isAuthenticated } = useAuthen();
+    const location = useLocation();
+
     return (
-        <main className={styles.layout}>
-            <div className='mainLayout'>
-                <section className={styles.heroSection}>
-                    <h1 className='font-hero mb5'>Login</h1>
-                    <div>
-                        <span className='font-md'>to continue to DevBlog</span>
+        isAuthenticated ? (
+            <Navigate to='/' state={{ from: location }} replace />
+        ) : (
+            <main>
+                <div className='mainLayout'>
+                    <div className={styles.layout}>
+                        <section className={styles.heroSection}>
+                            <h1 className='font-hero mb5'>Login</h1>
+                            <div>
+                                <span className='font-md'>to continue to DevBlog</span>
+                            </div>
+                        </section>
+                        <section className={styles.formSection}>
+                            <LoginForm/>
+                        </section>                       
                     </div>
-                </section>
-                <section className={styles.formSection}>
-                    <LoginForm/>
-                </section>   
-            </div>            
-        </main>
+                </div>            
+            </main>
+        )
     );
 };
 
