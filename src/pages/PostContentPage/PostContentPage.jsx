b6b43d4f23adf2f3f941103d0024a24a2b762c0e
Nick Yuen <138299516+NerdyClimber6969@users.noreply.github.com>
2025-06-13 17:42:03 +0800

    fix: improve comment pagination behavior - fix comments not respecting page size limit of 10 comments per page - fix pagination buttons not updating when adding comments to subsequent pages
    refactor: renaming the post and comments state to postState and commentState
    - improve state variable naming for better code readability and maintainability

--- src/pages/PostContentPage/PostContentPage.jsx
+++ src/pages/PostContentPage/PostContentPage.jsx
@@ -14,11 +14,14 @@ import { UnexpectedError, NotFoundError } from '../../components/Error';
 import styles from './PostContentPage.module.css';
 import btnStyles from '../../components/Button/Button.module.css';
 
+const NUMBER_OF_COMENTS = 10;
+const BASE_SEARCH_PARAMS = { pageSize: NUMBER_OF_COMENTS, orderBy: 'createdAt', orderDir: 'asc' };
+
 function PostContentPage() {
     const { handleApiCall } = useNotifications();
     const { isAuthenticated } = useAuthen();
-    const [post, setPost] = useState({ data: null, error: null, loading: true });
-    const [comments, setComments] = useState({ data: null, error: null, loading: true });
+    const [postState, setPostState] = useState({ data: null, error: null, loading: true });
+    const [commentState, setCommentState] = useState({ data: null, error: null, loading: true });
     const [isDialogOpen, setIsDialogOpen] = useState(false);
     const { postId } = useParams();
     const [commentPage, setCommentPage] = useState(1);
@@ -29,39 +32,44 @@ function PostContentPage() {
             notifyError: true,
             successMessage: 'New comment added sucessfully',
             onSuccess: (response) => {
-                setComments((current) => ({
-                    ...current, 
-                    data: [...current.data, response.comment]
-                }));
+                if (commentState.data.length < NUMBER_OF_COMENTS) {
+                    setCommentState((current) => ({
+                        ...current, 
+                        data: [...current.data, response.comment],
+                        total: current.total + 1
+                    }));
+
+                    return;
+                };
+
+                setCommentState({ ...commentState, total: commentState.total + 1 })
                 return;
             }
         })
     };
 
     function handleCommentPageChange(page) {
-        setComments({ ...comments, loading: true });
-        const searchParams = new URLSearchParams({ postId, page, pageSize: 3 });
+        setCommentState({ ...commentState, loading: true });
+        const searchParams = new URLSearchParams({ ...BASE_SEARCH_PARAMS, postId, page });
 
-        setTimeout(() => {
-            API.getComments(searchParams)
-                .then((result) => {
-                    setComments({ data: result.comments, error: null, loading: false, total: result.total })
-                    setCommentPage(page);
-                })
-                .catch((error) => setComments({ ...comments, data: null, error: error, loading: false }))
-        }, 1000)
+        API.getComments(searchParams)
+            .then((result) => {
+                setCommentState({ data: result.comments, error: null, loading: false, total: result.total })
+                setCommentPage(page);
+            })
+            .catch((error) => setCommentState({ ...commentState, data: null, error: error, loading: false }))
     };
 
     useEffect(() => {
         const fetchPostPageData = async () =>  {
-            const searchParams = new URLSearchParams({ postId, page: 1, pageSize: 3 })
+            const searchParams = new URLSearchParams({ ...BASE_SEARCH_PARAMS, postId, page: 1 })
 
             const [post, comments] = await Promise.allSettled([
                 API.getPost(postId).then((result) => ({ data: result.post })),
                 API.getComments(searchParams).then((result) => ({ data: result.comments, total: result.total }))
             ]);
 
-            const stateSetterMap = { post: setPost, comments: setComments };
+            const stateSetterMap = { post: setPostState, comments: setCommentState };
             const results = { post, comments };
 
             Object.entries(results).forEach(([name, result]) => {
@@ -82,7 +90,7 @@ function PostContentPage() {
         return;
     }, []);
     
-    if (!post.loading && post.error?.name === 'ResourceNotFoundError') {
+    if (!postState.loading && postState.error?.name === 'ResourceNotFoundError') {
         return (
             <main>
                 <div className='mainLayout'>
@@ -96,29 +104,29 @@ function PostContentPage() {
         <main>
             <div className='mainLayout'>                                          
                 <div className={`${styles.postSection} mb7`}>                   
-                    {post.loading && (<PostContentSkeleton/>)}
-                    {!post.loading && post.error && (<UnexpectedError mode='simple'/>)}                        
-                    {!post.loading && post.data && (                     
+                    {postState.loading && (<PostContentSkeleton/>)}
+                    {!postState.loading && postState.error && (<UnexpectedError mode='simple'/>)}                        
+                    {!postState.loading && postState.data && (                     
                         <PostContent
-                            title={post.data.title}
-                            summary={post.data.summary}
-                            author={post.data.author.username}
-                            content={post.data.content}
-                            createdAt={post.data.createdAt}
-                            dislike={post.data.dislike}
-                            like={post.data.like}
+                            title={postState.data.title}
+                            summary={postState.data.summary}
+                            author={postState.data.author.username}
+                            content={postState.data.content}
+                            createdAt={postState.data.createdAt}
+                            dislike={postState.data.dislike}
+                            like={postState.data.like}
                         />                                                          
                     )}                                                                     
                 </div>
             
                 <div className={styles.commentSection}>
-                    {comments.loading && (
+                    {commentState.loading && (
                         <div>
                             {Array(3).fill().map((_, index) => (<CommentSkeleton key={index}/>))}
                         </div>
                     )}
-                    {!comments.loading && comments.error && (<UnexpectedError mode='simple'/>)}
-                    {!comments.loading && comments.data && (
+                    {!commentState.loading && commentState.error && (<UnexpectedError mode='simple'/>)}
+                    {!commentState.loading && commentState.data && (
                         <>
                             <div className={styles.commentGroup}>
                                 {isDialogOpen && (
@@ -130,7 +138,7 @@ function PostContentPage() {
                                 )}
                                 <div className={`${styles.header} mb3`}>
                                     <h2 className='font-md'>
-                                        {`${comments.data.length > 1 ? 'Comments' : 'Comment'} (${comments.total})`}
+                                        {`${commentState.data.length > 1 ? 'Comments' : 'Comment'} (${commentState.total})`}
                                     </h2>
                                     {isAuthenticated && (
                                         <button 
@@ -141,9 +149,9 @@ function PostContentPage() {
                                         </button>
                                     )}
                                 </div>                                    
-                                {comments.data.length > 0 ? (
+                                {commentState.data.length > 0 ? (
                                     <ol>
-                                        {comments.data.map((comment) => (
+                                        {commentState.data.map((comment) => (
                                             <li key={comment.id}>
                                                 <Comment
                                                     author={comment.post.author.username}
@@ -159,10 +167,10 @@ function PostContentPage() {
                                     <p className='font-xs'>No Comment Yet !!!</p>
                                 )}
                             </div>
-                            {comments.data.length > 0 && (
+                            {commentState.data.length > 0 && (
                                 <Pagination 
                                     currentPage={commentPage}
-                                    totalPages={Math.max(Math.ceil(comments.total / 10), 1)}
+                                    totalPages={Math.max(Math.ceil(commentState.total / NUMBER_OF_COMENTS), 1)}
                                     maxVisiblePageBtn={5}
                                     onPageChange={handleCommentPageChange}
                                 />
